6f6366e230227fd67891792e9b4ad298205666ff
Samveg <thakersamveg608@gmail.com>
2019-03-01 19:43:40 +0530

    First commit

---
+++ app/src/main/java/com/sih/app1/kisaanmitra/model/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package com.sih.app1.kisaanmitra.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AuthenticationResponse extends GenericResponse implements Serializable {
+
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+}
