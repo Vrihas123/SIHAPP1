dad40a4e110a55559e1096e2955db9ba6e9e9a36
vrihas123 <vrihas007@gmail.com>
2019-03-02 10:18:55 +0530

    bottom nav added

---
+++ app/src/main/java/com/sih/app1/kisaanmitra/fragments/SuggestionFragment.java
@@ -0,0 +1,29 @@
+package com.sih.app1.kisaanmitra.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sih.app1.kisaanmitra.R;
+
+
+public class SuggestionFragment extends Fragment {
+
+
+    public SuggestionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_suggestion, container, false);
+        return view;
+    }
+
+}
